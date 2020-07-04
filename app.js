4217384dca9831eb9a17f5f900ee41b00e440e22
Karishma Sahoo <55288732+Karishmasahoo99@users.noreply.github.com>
2020-07-04 11:40:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const expressLayouts=require("express-ejs-layouts");
 const mongoose=require('mongoose');
 const flash=require('connect-flash');
 const session=require('express-session');
+const passport = require("passport");
 
 const app=express();
 
+//passport config
+require('./config/passport')(passport)
+
 //db config
-const db=require('./config/keys').MongoURI;
+const db=require('./config/keys').MongoURI;``
 
 //Connect to Mongo
  mongoose.connect(db,{ useNewUrlParser:true , useUnifiedTopology: true})
@@ -28,6 +32,10 @@ app.use(session({
   saveUninitialized:true
 }));
 
+//Passport MiddleWare after express session
+app.use(passport.initialize());
+app.use(passport.session()); 
+
 //Connect flash
 app.use(flash());
 
@@ -35,6 +43,7 @@ app.use(flash());
 app.use((req,res,next)=>{
   res.locals.success_msg=req.flash('success_msg');
   res.locals.error_msg=req.flash('error_msg');
+  res.locals.error=req.flash('error');
   next();
 })
 
